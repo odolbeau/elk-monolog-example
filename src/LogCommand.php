027891cc2330190a12aa55d0d5b912f5eecfdbb3
Olivier Dolbeau <contact@odolbeau.fr>
2015-11-14 12:56:49 +0100

    Send bermuda logs to logstash

--- src/LogCommand.php
+++ src/LogCommand.php
@@ -7,6 +7,10 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Monolog\Logger;
+use Monolog\Handler\GelfHandler;
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
 
 class LogCommand extends Command
 {
@@ -26,6 +30,28 @@ class LogCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Hello world!</info>');
+        $output->writeln('<info>Let\'s wear a <comment>bermuda</comment>!</info>');
+
+        $logger = $this->getLogger();
+        $logger->warning('I wear a bearmuda.');
+    }
+
+    /**
+     * getLogger
+     *
+     * @return LoggerInterface
+     */
+    protected function getLogger()
+    {
+        $logger = new Logger('demonstration');
+        $logger->pushHandler(
+            new GelfHandler(
+                new Publisher(
+                    new UdpTransport('127.0.0.1', 12201)
+                )
+            )
+        );
+
+        return $logger;
     }
 }
